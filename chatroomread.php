ca12584d6ef40b7c1bd9df5ce4dfc466a76d0a08
Justin Randell <justinrandell@38580.no-reply.drupal.org>
2006-06-26 05:37:02 +0000

    change to a full bootstrap

--- chatroomread.php
+++ chatroomread.php
@@ -17,11 +17,11 @@ if (!isset($_GET['timestamp'])    ||
 } 
 
 // need to get the path from the webroot, so that multi-site installs work
-if ($_SERVER['PHP_SELF'] == '/chatroom.php') {
+if ($_SERVER['PHP_SELF'] == '/chatroomread.php') {
   $site_path = '';
 }
 else {
-  $site_path = substr($_SERVER['PHP_SELF'], 1, strlen('/chatroom.php'));
+  $site_path = substr($_SERVER['PHP_SELF'], 1, strlen('/chatroomread.php'));
   $site_path = str_replace(array('/', '.', ' '), array('_', '_', '-'), $site_path) .'_';
 }
 
@@ -33,7 +33,6 @@ $last_msg_id       = $_GET['last_msg_id'];
 
 // setup path to file
 $cache_file = './modules/chatroom/chat_cache/'. $site_path . 'chat_' . $chat_id;
-//echo "/** $cache_file **/";
 
 // compare the cache files timestamp to the browsers timestamp
 if (@file_exists($cache_file)) {
@@ -43,14 +42,14 @@ if (@file_exists($cache_file)) {
     header("Cache-Control: no-store, no-cache, must-revalidate");
     header("Cache-Control: post-check=0, pre-check=0", false);
     header("Pragma: no-cache");
-    echo '[[],[]]';
+    echo '';
     exit;
   }
 }
 
 // cache miss, so bootstrap drupal
 require_once './includes/bootstrap.inc';
-drupal_bootstrap(DRUPAL_BOOTSTRAP_PATH);
+drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 
 // invoke the chatroom read messages code and exit
 module_invoke('chatroom', 'chat_read_msgs_cache', $chat_id, $last_msg_id, $update_count);
