e5aa8e2a610b642454d3e4d56787fca48f3e2639
Justin Randell <justinrandell@38580.no-reply.drupal.org>
2009-10-30 17:53:38 +0000

    ahem, add the popout template

---
+++ page-chat-popout.tpl.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language ?>" lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>">
+  <head>
+    <?php print $head ?>
+    <title><?php print $head_title ?></title>
+    <?php print $styles ?>
+    <?php print $scripts ?>
+    <!--[if lt IE 7]>
+      <?php print phptemplate_get_ie_styles(); ?>
+    <![endif]-->
+  </head>
+  <body<?php print phptemplate_body_class($left, $right); ?>>
+  <div id="content">
+    <?php print $content ?>
+  </div>
+<!-- /layout -->
+  <?php print $closure ?>
+  </body>
+</html>
